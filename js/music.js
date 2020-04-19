929aff37e250ae43ef91f5c94b140b1d8f1ae81a
DLiECHO <1348288729@qq.com>
2020-04-19 14:02:26 +0800

    .top

---
+++ js/music.js
@@ -0,0 +1,52 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelector('main').className += 'loaded';
+// });
+alert('本页面并没有为不同设备、浏览器做适配工作\r\n页面右侧动画点击分别为bgm的暂停和继续\r\nbgm：林俊杰-伟大的渺小');
+setTimeout(function() {
+    var e = document.createEvent("MouseEvents");
+    e.initEvent("click", true, true);
+    document.getElementById("clnav").dispatchEvent(e);
+}, 500);
+
+setTimeout(function() {
+        var e = document.createEvent("MouseEvents");
+        e.initEvent("click", true, true);
+        document.getElementById("cl").dispatchEvent(e);
+}, 0);
+
+// function start() {
+//     var au = document.getElementById('audio');
+//     au.play();
+//     var circle1 = document.getElementById('circle1');
+//     circle1.style.display='none';
+//     var circle2 = document.getElementById('circle2');
+//     circle2.style.display="block";
+// }
+
+function Play() {
+    var au = document.getElementById('audio');
+    au.play();
+    var circle1 = document.getElementById('circle1');
+    setTimeout("circle1.style.transition='opacity 1s'", 0);
+    setTimeout("circle1.style.opacity=0", 0);
+    setTimeout("circle1.style.display='none'", 1000);
+    var circle2 = document.getElementById('circle2');
+    setTimeout("circle2.style.display='block';", 00);
+    setTimeout("circle2.style.opacity=0;", 00);
+    setTimeout("circle2.style.transition='opacity 2s'", 1);
+    setTimeout("circle2.style.opacity=1", 500);
+}
+
+function Pause() {
+    var au = document.getElementById('audio');
+    au.pause();
+    var circle1 = document.getElementById('circle2');
+    setTimeout("circle2.style.transition='opacity 1s'", 0);
+    setTimeout("circle2.style.opacity=0", 0);
+    setTimeout("circle2.style.display='none'", 1000);
+    var circle2 = document.getElementById('circle1');
+    setTimeout("circle1.style.display='block';", 0);
+    setTimeout("circle1.style.opacity=0;", 0);
+    setTimeout("circle1.style.transition='opacity 2s'", 1);
+    setTimeout("circle1.style.opacity=1", 500);
+}
